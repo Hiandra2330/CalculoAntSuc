8aa5301b7f762e8a6fd01c558e8ef770a1bcbd68
Hiandra2330 <100353954+Hiandra2330@users.noreply.github.com>
2022-03-19 23:46:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+entrada = input('digite um numero ou "sair" para encerrar\n')
+while entrada != 'sair': 
+  numero = int(entrada)
+
+  ant = numero -1
+  suc = numero +1
+
+  print('o sucessor de ' + str(numero) + ' é ' + str(suc)) 
+  print('o antecessor de ' + str(numero) + ' é ' + str(ant))
+  entrada = input('digite um numero ou "sair" para encerrar\n') 
+print('programa encerrado!')
